acd4bf0a35f3c8e93c2ee4363c9d2c20fa5e0d1c
Matheus Branco Borella <dark.ryu.550@gmail.com>
2022-04-22 23:43:20 -0300

    Update the IDCT shader

--- vpr/src/compute/IDCT.glsl
+++ vpr/src/compute/IDCT.glsl
@@ -5,18 +5,17 @@
 ///
 ///
 
-
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
-layout(bind = 0, location = 0, rgba32i) restrict readonly  uniform image2D CoefficientImage;
-layout(bind = 0, location = 1, rgba32i) restrict writeonly uniform image2D ComponentImage;
+layout(set = 0, binding = 0, rgba32f) restrict readonly  uniform image2D CoefficientImage;
+layout(set = 0, binding = 1, rgba32f) restrict writeonly uniform image2D ComponentImage;
 
 const float C[8] = float[8](0.707106781187, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0);
 const float PI = 3.141592653589;
 
 void main()
 {
-	uvec2 pos = gl_LocalInvocationID.xy;
-	uvec2 gbl = gl_GlobalInvocationID.xy;
+	ivec2 pos = ivec2(gl_LocalInvocationID.xy);
+	ivec2 gbl = ivec2(gl_GlobalInvocationID.xy);
 
 	vec4 acc = vec4(0.0);
 	for(int i = 0; i < 8; ++i)
@@ -30,6 +29,14 @@ void main()
 		}
 	}
 	acc *= 0.25;
+
+	/* At this point, the value in the accumulator is in the [-256, 255] range,
+	 * and, our output frame expects is expected to contain 32bit floating point
+	 * color data, in the [0.0, 1.0] range.
+	 *
+	 * The last step of the decoding process is to map the former range into the
+	 * latter, then, we simply store the result in the target YCbCrA image. */
+	acc = clamp((acc + 256.0) / 512.0, 0.0, 1.0);
 
 	imageStore(ComponentImage, gbl, acc);
 }
